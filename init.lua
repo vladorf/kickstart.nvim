8351d5051140810cfd1719b160f16f725e110e88
Vladimir Uzik <vlado.uzik@gmail.com>
2025-08-28 00:43:20 +0200

    Add <leader>fd shortcut for finding files in current dir

--- init.lua
+++ init.lua
@@ -426,9 +426,13 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      local utils = require 'telescope.utils'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>fd', function()
+        builtin.find_files { cwd = utils.buffer_dir() }
+      end, { desc = '[S]earch [D]irectory' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
